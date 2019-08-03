72cb5f3773a109c52cdc25f5622d2b1b124ea9b1
seb <ubdfj@student.kit.edu>
2019-08-03 12:55:17 +0200

    Added patroen and paypal.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,16 @@ const IndexPage = () => (
       <p>Es freut mich, dass Du hierher gefunden hast!</p>
       <p>Hier findest Du einen Teil meiner Arbeiten über digitale und analoge Kunst sowie Filme, Animationen und Interaktives.
       Informiere dich über meine regelmäßigen Livestreams und Veranstaltungen wo ich persönlich anzutreffen bin!</p>
-      <p>Folge mir über Plattformen wie <a href="https://twitter.com/DKopanitsak">Twitter</a>, <a href="https://www.facebook.com/TheArtOfDeborahKopanitsak/">Facebook</a>, <a href="https://www.instagram.com/deborahkopanitsak/">Instagram</a>, <a href="https://www.youtube.com/channel/UCF6HQAA3kOt15M-sMK3Ua5A">Youtube</a>, <a href="https://www.twitch.tv/kopanitsak">Twitch</a>, <a href="https://www.deviantart.com/kopanitsak">DeviantArt</a> oder <a href="http://www.toonsup.com/kopanitsak">toonsUp</a> um immer aktuell über meine Arbeit informiert zu werden.</p>
+      <p>Folge mir über Plattformen wie <a href="https://twitter.com/DKopanitsak">Twitter</a>, 
+        <a href="https://www.facebook.com/TheArtOfDeborahKopanitsak/">Facebook</a>, 
+        <a href="https://www.instagram.com/deborahkopanitsak/">Instagram</a>, 
+        <a href="https://www.youtube.com/channel/UCF6HQAA3kOt15M-sMK3Ua5A">Youtube</a>, 
+        <a href="https://www.twitch.tv/kopanitsak">Twitch</a>, 
+        <a href="https://www.deviantart.com/kopanitsak">DeviantArt</a> oder 
+        <a href="http://www.toonsup.com/kopanitsak">toonsUp</a> um immer aktuell über meine Arbeit informiert zu werden.
+        Für exklusiven Inhalt werde mein Patron auf <a href="https://www.patreon.com/kopanitsak">Patreon</a> oder unterstütze mich einfach auf <a href="https://www.paypal.me/DKopanitsak">Paypal</a>.
+      </p>
+      <p>Viel Spaß!</p>
     </div>
 
     <h1>Kontakt</h1>
